eb596d389251e9fc16b2621c4436e820afa3fcdd
TirumaniN <tirumani.nadar@acetrot.com>
2022-02-22 23:41:08 +0530

    projects page added

---
+++ projects.php
@@ -0,0 +1,51 @@
+<?php include('header.php') ?>
+<?php include('nav.php') ?>
+
+<main class="content">
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb" style="background-image: linear-gradient(to right, #145bdd, #0ab90acc); border-radius: 0px 0px 30px 30px;">
+            <li class="breadcrumb-item h1 text-white">Projects I Worked On</li>
+        </ol>
+    </nav>
+</main>
+
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col-md-3 mb-20">
+                <div class="card shadow-sm">
+                    <img src="images/okidos-logo.png" class="img-fit-contain" width="100%" height="225" alt="Okidos">
+                </div>
+            </div>
+            <div class="col-md-3 mb-20">
+                <div class="card shadow-sm">
+                    <img src="images/skyline-logo.png" class="img-fit-contain" width="100%" height="225" alt="">
+                </div>
+            </div>
+            <div class="col-md-3 mb-20">
+                <div class="card shadow-sm">
+                    <img src="images/spaces-logo.png" class="img-fit-contain" width="100%" height="225" alt="">
+                </div>
+            </div>
+            <div class="col-md-3 mb-20">
+                <div class="card shadow-sm">
+                    <img src="images/nmmt-logo.png" class="img-fit-contain" width="100%" height="225" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col-md-3 mb-20">
+                <div class="card shadow-sm">
+                    <img src="images/covid-relief-logo.png" class="img-fit-contain" width="100%" height="225" alt="">
+                </div>
+            </div>
+            <div class="col-md-3 mb-20">
+                <div class="card shadow-sm">
+                    <img src="images/basta-logo.png" class="img-fit-contain" width="100%" height="225" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php') ?>
