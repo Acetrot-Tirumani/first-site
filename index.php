1bbb35ed45635e25316cfa60d1366da15ca4b1ac
TirumaniN <tirumani.nadar@acetrot.com>
2022-02-24 22:52:24 +0530

    mobile nav open fixes

--- index.php
+++ index.php
@@ -97,8 +97,6 @@
         </div>
     </div>
 </main>
-<!-- ✅ load Bootstrap JS ✅ -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script>
     $('.carousel').carousel({
         interval: 15000
